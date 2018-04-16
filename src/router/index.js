6038ee345ff12e09bf3dd0c279c378cd69d708f8
lfHDL <475830965@qq.com>
2018-04-16 21:39:57 +0800

    子路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login'
 import Home from '@/components/home/home'
+import UserList from '@/components/user-list/user-list'
+import RoleList from '@/components/role-list/role-list'
 
 Vue.use(Router)
 
@@ -15,7 +17,19 @@ const router = new Router({
     {
       name: 'home',
       path: '/',
-      component: Home
+      component: Home,
+      children: [
+        {
+          name: 'user-list',
+          path: '/users',
+          component: UserList
+        },
+        {
+          name: 'role-list',
+          path: '/roles',
+          component: RoleList
+        }
+      ]
     }
   ]
 })
